92e365ff30fffaa995e2e10de2089f9287fee2ef
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2015-03-28 23:49:21 +0200

    attempt to add service check

--- scripts/register.py
+++ scripts/register.py
@@ -9,15 +9,18 @@ parser.add_argument('-mp','--mongo-port',help='MongoDB Node port', required=True
 parser.add_argument('-ci','--consul-ip',help='Consul Agent IP Address', required=True)
 parser.add_argument('-cp','--consul-port',help='Consul Agent Active Port', required=True)
 args = parser.parse_args()
+c = consul.Consul(host=args.consul_ip, port=args.consul_port, dc="dc1")
 
 def register_mongo(ip, hostname, port, role):
-    catalog = consul.Consul(host=args.consul_ip, port=args.consul_port, dc="dc1").catalog
+    catalog = c.catalog
     dc = "dc1"
-    service=dict()
-    service["Service"] = "MongoDB"
-    service["ID"] = hostname
-    service["Tags"] = [role]
-    service["Port"] = int(port)
-    catalog.register(hostname,ip,service=service,dc=dc)
+    service={"Service":"MongoDB", "ID":hostname, "Tags":[role], "Port":int(port)}
+    check={"Node": hostname,"CheckID": "service:MongoDB","Name": "MongoDB health check","Notes": "MongoDB health check","Status": "passing","ServiceID": hostname}
+    catalog.register(hostname,ip,service=service,check=check, dc=dc,)
+
+def add_mongo_health_check(mongo_ip, mongo_port):
+    check = c.Agent().Check()
+    check.register("MongoDB Service", check_id="up", http="http://"+mongo_ip+":"+str(mongo_port), interval="10s", notes="Check The mongodb is up or not")
+
 
 register_mongo(args.mongo_ip, args.mongo_hostname,args.mongo_port,args.mongo_role)
